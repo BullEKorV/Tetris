60d9e7f5a92f94a220bc2cb3cc0050ca238d0f0c
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2022-04-08 22:25:49 +0200

    MASSIVE CHANGES

--- Tetris/Board.cs
+++ Tetris/Board.cs
@@ -5,7 +5,7 @@ public class Board
     float timeTillNextUpdate;
     public Board()
     {
-        grid = new Block[10, 20];
+        grid = new Block[10, 21];
         timeTillNextUpdate = timeBetweenUpdate;
 
         grid[5, 2] = new Block(Color.BLACK);
@@ -24,9 +24,17 @@ public class Board
             playerMove = Move.Left;
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_D))
             playerMove = Move.Right;
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+            playerMove = Move.Down;
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+            playerMove = Move.InstaDown;
 
-        if (playerMove != Move.None)
-            MoveActiveBlocks(playerMove);
+        // DEBUG
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_C))
+            SpawnPiece();
+
+        // Commit to player move
+        MoveActiveBlocks(playerMove);
 
         // Perform a "game tick"
         if (timeTillNextUpdate <= 0)
@@ -35,10 +43,89 @@ public class Board
             timeTillNextUpdate = timeBetweenUpdate;
         }
     }
+    #region Spawning
+    private void SpawnPiece()
+    {
+        (int, int)[] piece = GetPiece();
+        Color color = Color.BEIGE;
+
+        int pieceCenterX = (int)MathF.Ceiling((float)GetPieceWidth(piece) / 2);
+
+        // Actually place block
+        foreach ((int, int) block in piece)
+        {
+            grid[(grid.GetLength(0) / 2) - pieceCenterX + block.Item1, block.Item2] = new Block(color);
+        }
+
+        timeTillNextUpdate = timeBetweenUpdate;
+    }
+    private int GetPieceWidth((int, int)[] piece)
+    {
+        int width = 0;
+        foreach ((int, int) block in piece)
+        {
+            width = block.Item1 > width ? block.Item1 : width;
+        }
+        return width + 1; // + 1 because array ints start at 0
+    }
+    private int GetPieceHeight((int, int)[] piece)
+    {
+        int height = 0;
+        foreach ((int, int) block in piece)
+        {
+            height = block.Item2 > height ? block.Item2 : height;
+        }
+        return height;
+    }
+
+    private (int, int)[] GetPiece()
+    {
+        (int, int)[][] allPieces = {
+           new (int,int)[] // line piece
+           {(0,0),
+            (1,0),
+            (2,0),
+            (3,0)},
+            new (int,int)[] // square piece
+           {(0,0),
+            (1,0),
+            (0,1),
+            (1,1)},
+            new (int,int)[] // L piece right
+           {(0,1),
+            (1,1),
+            (2,1),
+            (2,0)},
+            new (int,int)[] // L piece left
+           {(0,0),
+            (0,1),
+            (1,1),
+            (2,1)},
+            new (int,int)[] // Squiggly piece left
+           {(0,0),
+            (1,0),
+            (1,1),
+            (2,1)},
+            new (int,int)[] // Squiggly piece right
+           {(0,1),
+            (1,1),
+            (1,0),
+            (2,0)}
+        };
+
+        Random rnd = new Random();
+        (int, int)[] piece = allPieces[rnd.Next(0, allPieces.Length)];
+
+
+        return piece;
+    }
+    #endregion
+    #region Moving blocks
     private void MoveActiveBlocks(Move move)
     {
         Dictionary<(int, int), (int, int)> blocksToMove = new Dictionary<(int, int), (int, int)>(); // Lists of all blocks that has already been moved so we dont accidentally move same block twice
 
+        // Get the desired new location for all of the blocks
         for (int x = 0; x < grid.GetLength(0); x++)
         {
             for (int y = 0; y < grid.GetLength(1); y++)
@@ -55,43 +142,23 @@ public class Board
             }
         }
 
-        bool validMove = true;
-        for (int x = 0; x < grid.GetLength(0); x++)
-        {
-            for (int y = 0; y < grid.GetLength(1); y++)
-            {
-                if (blocksToMove.ContainsKey((x, y)))
-                {
-                    if (blocksToMove[(x, y)].Item1 > grid.GetLength(0) - 1 || blocksToMove[(x, y)].Item1 < 0)
-                    {
-                        validMove = false;
-                        continue;
-                    }
-                    if (blocksToMove[(x, y)].Item2 > grid.GetLength(1) - 1)
-                    {
-                        validMove = false;
-                        PieceIsDone(blocksToMove);
-                        grid[0, 0] = new Block(Color.GOLD);
-                        return;
-                    }
-                    if (grid[blocksToMove[(x, y)].Item1, blocksToMove[(x, y)].Item2] != null && !grid[blocksToMove[(x, y)].Item1, blocksToMove[(x, y)].Item2].isActive)
-                    {
-                        validMove = false;
-                        if (blocksToMove[(x, y)].Item2 > y)
-                        {
-                            PieceIsDone(blocksToMove);
-                            grid[0, 0] = new Block(Color.GOLD);
-                            grid[1, 0] = new Block(Color.GOLD);
+        // For the insta down move
+        if (move == Move.InstaDown)
+            blocksToMove = GetPiecePositionInstaDown();
 
-                            return;
-                        }
-                    }
-                }
+        // Find color of the piece
+        Color pieceColor = new Color();
+        foreach (Block block in grid)
+        {
+            if (block != null && block.isActive)
+            {
+                pieceColor = block.color;
+                break; /// ???? DOES IT REALLY BREAK?!?!=!?!
             }
         }
 
-
-        if (validMove)
+        // Replace old blocks with the new blocks aka move them
+        if (ValidMove(blocksToMove))
         {
             foreach (var item in blocksToMove)
             {
@@ -99,23 +166,99 @@ public class Board
             }
             foreach (var item in blocksToMove)
             {
-                grid[item.Value.Item1, item.Value.Item2] = new Block(Color.BEIGE);
+                grid[item.Value.Item1, item.Value.Item2] = new Block(pieceColor);
             }
         }
     }
+    private Dictionary<(int, int), (int, int)> GetPiecePositionInstaDown()
+    {
+        Dictionary<(int, int), (int, int)> blocksToMove = new Dictionary<(int, int), (int, int)>(); // Lists of all blocks that has already been moved so we dont accidentally move same block twice
+
+        // Find y distance to move all blocks
+        int smallestDeltaY = grid.GetLength(1);
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                if (grid[x, y] != null && grid[x, y].isActive)
+                {
+                    if (DeltaYToBlockUnder((x, y)) < smallestDeltaY)
+                        smallestDeltaY = DeltaYToBlockUnder((x, y));
+                }
+            }
+        }
+
+        // Find blocks and new coords
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                if (grid[x, y] != null && grid[x, y].isActive)
+                {
+                    blocksToMove.Add((x, y), (x, y + smallestDeltaY));
+                }
+            }
+        }
+        return blocksToMove;
+    }
+    private int DeltaYToBlockUnder((int, int) block)
+    {
+        for (int y = block.Item2; y < grid.GetLength(1); y++)
+        {
+            if (grid[block.Item1, y] != null && !grid[block.Item1, y].isActive)
+                return y - block.Item2 - 1; // Find blocks BETWEEN this and block under
+        }
+        return grid.GetLength(1) - block.Item2 - 1; // If no blocks under all, just find distance to floor
+    }
+    private bool ValidMove(Dictionary<(int, int), (int, int)> blocksToMove)
+    {
+        // Check if the blocks can be moved
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                if (blocksToMove.ContainsKey((x, y)))
+                {
+                    // Check if colliding with walls
+                    if (blocksToMove[(x, y)].Item1 > grid.GetLength(0) - 1 || blocksToMove[(x, y)].Item1 < 0)
+                    {
+                        return false;
+                    }
+                    // Check if at bottom
+                    if (blocksToMove[(x, y)].Item2 > grid.GetLength(1) - 1)
+                    {
+                        PieceIsDone(blocksToMove);
+                        return false;
+                    }
+                    // Check if inactive block at location
+                    if (grid[blocksToMove[(x, y)].Item1, blocksToMove[(x, y)].Item2] != null && !grid[blocksToMove[(x, y)].Item1, blocksToMove[(x, y)].Item2].isActive)
+                    {
+                        // Piece is done if inactive block underneath
+                        if (blocksToMove[(x, y)].Item2 > y)
+                            PieceIsDone(blocksToMove);
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
     private void PieceIsDone(Dictionary<(int, int), (int, int)> completedPiece)
     {
-        foreach (var item in completedPiece)
+        foreach (Block block in grid)
         {
-            grid[item.Key.Item1, item.Key.Item2] = new Block(Color.BLACK) { isActive = false };
+            if (block != null)
+                block.isActive = false;
         }
     }
+    #endregion
+    #region Drawing
     public void Draw()
     {
         int blockSize = Raylib.GetScreenHeight() / 24;
 
         // Draw arena
-        Rectangle arena = new Rectangle((Raylib.GetScreenWidth() - blockSize * grid.GetLength(0)) * 0.5f, 0, blockSize * grid.GetLength(0), blockSize * grid.GetLength(1));
+        Rectangle arena = new Rectangle((Raylib.GetScreenWidth() - blockSize * grid.GetLength(0)) * 0.5f, -blockSize * 0.5f, blockSize * grid.GetLength(0), blockSize * grid.GetLength(1));
         Raylib.DrawRectangleLinesEx(arena, 5, Color.BLUE);
 
         // Draw individual blocks
@@ -129,30 +272,8 @@ public class Board
                 }
             }
         }
-
     }
-
-    public void PlaceNewPiece()
-    {
-        (int, int)[] shape = GetPiece();
-        Color color = Color.BEIGE;
-
-        foreach ((int x, int y) places in shape)
-        {
-            grid[places.y, places.x] = new Block(color);
-        }
-    }
-
-    private (int, int)[] GetPiece()
-    {
-        (int, int)[] test = {
-            (0,0),
-            (0,1),
-            (0,2),
-            (0,3)
-        };
-        return test;
-    }
+    #endregion   
 }
 enum Move
 {
